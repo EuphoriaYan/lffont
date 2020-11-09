8e60036d8a5a3d1daa778848d163e765fc258f8e
EuphoriaYan <ysq58000@foxmail.com>
2020-11-09 15:58:36 +0800

    update

--- convert_json.py
+++ convert_json.py
@@ -11,5 +11,13 @@ if __name__ == '__main__':
     args = parse.parse_args()
     with open(args.src_json, 'r', encoding='utf-8') as fp:
         font_missing_json = json.load(fp)
+    # ['font_name', 'font_pth', 'missing', 'fake']
     dst_json = dict()
-    pprint(font_missing_json[0].keys())
+    s = '您今天吃了吗为打分卡哭了染发大杀四方抢位置婆牛网'
+    for font_json in font_missing_json:
+        font_json_new = dict()
+        font_json_new['path'] = font_json['font_pth']
+        font_json_new['charlist'] = list(s)
+        dst_json[font_json['font_name']] = font_json_new
+    with open(args.dst_json, 'w', encoding='utf-8') as fp:
+        json.dump(dst_json, fp)
